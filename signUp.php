66894f0ae7b1cf2ecfe48d00065723150de6d19f
Erik <e.vandervegt03@gmail.com>
2018-01-11 16:01:25 +0100

    tweede

---
+++ signUp.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title> Simple_Shopping_cart</title>
+        <link rel =" stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+        <link rel =" stylesheet" href="cart.css"/>
+        
+        
+    </head>
+    <body>
+        <div class="menu">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a href="index.php" class="navbar-brand" title="PHP Computer store Home " style="padding-top: 12px ;font-family: Georgia ">PHP Developers Store</a>
+                </div>
+                <div>
+                    <ul class="nav navbar-nav navbar-right">
+                        
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"style="padding-top: 10px"></span> Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+      <body>
+
+    
+
+</body>
+</html>
+  
